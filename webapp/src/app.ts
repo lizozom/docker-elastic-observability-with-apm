08bf87469dca260f5cf50f280a3c848b664986fc
lizozom <liza.katz@elastic.co>
2022-12-19 21:57:41 +0200

    connect with APM

--- webapp/src/app.ts
+++ webapp/src/app.ts
@@ -1,18 +1,19 @@
 import express from 'express';
-import otel from "@opentelemetry/api";
 import './otel';
+import otel from "@opentelemetry/api";
 
 const app = express();
 const port = 3000;
 
+const myMeter = otel.metrics.getMeter(
+  'my-service-meter'
+);
+const counter = myMeter.createCounter('home-route-counter', {
+  description: 'Home run counter',
+});
+
 app.get('/', (req, res) => {
   console.log("CALLED")
-  const myMeter = otel.metrics.getMeter(
-    'my-service-meter'
-  );
-  const counter = myMeter.createCounter('home-route-counter', {
-    description: 'Home run counter',
-  });
   const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress 
   counter.add(1, {
     'user.ip': ip,
@@ -21,6 +22,5 @@ app.get('/', (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
   return console.log(`Express is listening at http://localhost:${port}`);
 });
--- webapp/src/otel.ts
+++ webapp/src/otel.ts
@@ -1,19 +1,19 @@
 import * as opentelemetry from "@opentelemetry/sdk-node";
-import { BatchSpanProcessor, ConsoleSpanExporter } from "@opentelemetry/sdk-trace-base";
 import { Resource } from "@opentelemetry/resources";
 import { SemanticResourceAttributes } from "@opentelemetry/semantic-conventions";
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { diag, DiagConsoleLogger, DiagLogLevel, metrics } from "@opentelemetry/api";
-import { ConsoleMetricExporter, MeterProvider, PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+import { MeterProvider, PeriodicExportingMetricReader } from "@opentelemetry/sdk-metrics";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { OTLPMetricExporter } from "@opentelemetry/exporter-metrics-otlp-http";
 
-console.log("---------------------------------------------------------------")
 console.log(process.env.OTEL_EXPORTER_OTLP_ENDPOINT, process.env.OTEL_EXPORTER_OTLP_HEADERS)
 
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.DEBUG);
 
 // configure the SDK to export telemetry data to the console
 // enable all auto-instrumentations from the meta package
-const traceExporter = new ConsoleSpanExporter();
+const traceExporter = new OTLPTraceExporter();
 
 const resource = new Resource({
   [SemanticResourceAttributes.SERVICE_NAME]: "demo-webapp",
@@ -42,10 +42,10 @@ process.on('SIGTERM', () => {
 // Configure metrics
 
 const metricReader = new PeriodicExportingMetricReader({
-  exporter: new ConsoleMetricExporter(),
+  exporter: new OTLPMetricExporter(),
 
   // Default is 60000ms (60 seconds). Set to 3 seconds for demonstrative purposes only.
-  exportIntervalMillis: 3000,
+  exportIntervalMillis: 10000,
 });
 
 const myServiceMeterProvider = new MeterProvider({
@@ -55,15 +55,5 @@ const myServiceMeterProvider = new MeterProvider({
 myServiceMeterProvider.addMetricReader(metricReader);
 metrics.setGlobalMeterProvider(myServiceMeterProvider)
 
-
-// // Configure tracer
-// const provider = new NodeTracerProvider({
-//   resource: resource,
-// });
-// const exporter = new ConsoleSpanExporter();
-// const processor = new BatchSpanProcessor(exporter);
-// provider.addSpanProcessor(processor);
-
-// provider.register();
 
 export default sdk;
